8910a1c832142c320bb496cf691ec629118a10d8
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-07-10 10:59:22 -0300

    feat: init

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,87 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import dayjs from "dayjs";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
+        }),
+      },
+    },
+    async (req, res) => {
+      const {
+        destination,
+        ends_at,
+        starts_at,
+        owner_email,
+        owner_name,
+        emails_to_invite,
+      } = req.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date");
+      }
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email };
+                }),
+              ],
+            },
+          },
+        },
+      });
+
+      const mail = await getMailClient();
+
+      const message = await mail.sendMail({
+        from: {
+          name: "Equipe plann.er",
+          address: "oi@plann.er",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: "Testando envio de email",
+        html: `<p>Testando o envio de emails da aplicação!</p>`,
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { tripId: trip.id };
+    }
+  );
+
+  app.get("/trips", async (req, res) => {});
+}
